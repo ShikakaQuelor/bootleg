c16fa39ecb6ec493dbfd4d611707483f41537531
Quelor <quelor.jb@gmail.com>
2024-08-24 08:14:04 +0200

    fix: tailwind colors now extend instead of overwrite

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,11 @@ module.exports = {
       backgroundImage: {
         "cone-gradient": "conic-gradient(from 90deg, green, orange, red)",
       },
-    },
-    colors: {
-      "dark-green": "#265642",
-      "light-green": "#34783B",
-      "gold-yellow": "#F8D648",
+      colors: {
+        "dark-green": "#265642",
+        "light-green": "#34783B",
+        "gold-yellow": "#F8D648",
+      },
     },
   },
   plugins: [],
