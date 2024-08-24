0d7fcc7a97398e8ae453c6adcc6ee8a5a0b304a4
Quelor <quelor.jb@gmail.com>
2024-08-24 08:16:36 +0200

    fix: faulty type conversion for sprit calc
    also updates types, types could be slimmed down.

--- internals/taxes/taxCalc.go
+++ internals/taxes/taxCalc.go
@@ -4,103 +4,109 @@ import (
 	"slices"
 )
 
+type ImageModule struct {
+	Extensions interface{} `json:"extensions,omitempty" form:"extensions"`
+	ProductID  interface{} `json:"productId,omitempty" form:"productId"`
+	Sizes      interface{} `json:"sizes,omitempty" form:"sizes"`
+	Thumbnail  interface{} `json:"thumbnail,omitempty" form:"thumbnail"`
+}
+
+type Image struct {
+	FileType interface{} `json:"fileType,omitempty" form:"fileType"`
+	ImageURL string      `json:"imageUrl,omitempty" form:"imageUrl"`
+	Size     interface{} `json:"size,omitempty" form:"size"`
+}
+
+type TasteClock struct {
+	Key   string `json:"key,omitempty" form:"key"`
+	Value int    `json:"value,omitempty" form:"value"`
+}
+
 type Product struct {
-	AlcoholPercentage   float64       `json:"alcoholPercentage"`
-	AlcoholTax          float64       `json:"alcoholTax"`
-	Assortment          string        `json:"assortment"`
-	AssortmentText      string        `json:"assortmentText"`
-	BottleText          string        `json:"bottleText"`
-	Category            interface{}   `json:"category"`
-	CategoryLevel1      string        `json:"categoryLevel1"`
-	CategoryLevel2      string        `json:"categoryLevel2"`
-	CategoryLevel3      string        `json:"categoryLevel3"`
-	CategoryLevel4      interface{}   `json:"categoryLevel4"`
-	Color               string        `json:"color"`
-	Country             string        `json:"country"`
-	CustomCategoryTitle string        `json:"customCategoryTitle"`
-	DishPoints          interface{}   `json:"dishPoints"`
-	EthicalLabel        interface{}   `json:"ethicalLabel"`
-	Grapes              []interface{} `json:"grapes"`
-	ImageModules        struct {
-		Extensions interface{} `json:"extensions"`
-		ProductID  interface{} `json:"productId"`
-		Sizes      interface{} `json:"sizes"`
-		Thumbnail  interface{} `json:"thumbnail"`
-	} `json:"imageModules"`
-	Images []struct {
-		FileType interface{} `json:"fileType"`
-		ImageURL string      `json:"imageUrl"`
-		Size     interface{} `json:"size"`
-	} `json:"images"`
-	IsAlcoholFree                   bool        `json:"isAlcoholFree"`
-	IsBsAssortment                  bool        `json:"isBsAssortment"`
-	IsClimateSmartPackaging         bool        `json:"isClimateSmartPackaging"`
-	IsCompletelyOutOfStock          bool        `json:"isCompletelyOutOfStock"`
-	IsDiscontinued                  bool        `json:"isDiscontinued"`
-	IsEthical                       bool        `json:"isEthical"`
-	IsFsAssortment                  bool        `json:"isFsAssortment"`
-	IsFsTsAssortment                bool        `json:"isFsTsAssortment"`
-	IsKosher                        bool        `json:"isKosher"`
-	IsManufacturingCountry          bool        `json:"isManufacturingCountry"`
-	IsNews                          bool        `json:"isNews"`
-	IsOrganic                       bool        `json:"isOrganic"`
-	IsPaAssortment                  bool        `json:"isPaAssortment"`
-	IsRecommendedByTasteProfile     bool        `json:"isRecommendedByTasteProfile"`
-	IsRegionalRestricted            bool        `json:"isRegionalRestricted"`
-	IsSupplierTemporaryNotAvailable bool        `json:"isSupplierTemporaryNotAvailable"`
-	IsSustainableChoice             bool        `json:"isSustainableChoice"`
-	IsTemporaryOutOfStock           bool        `json:"isTemporaryOutOfStock"`
-	IsTsAssortment                  bool        `json:"isTsAssortment"`
-	IsTsLsAssortment                bool        `json:"isTsLsAssortment"`
-	IsTseAssortment                 bool        `json:"isTseAssortment"`
-	IsTssAssortment                 bool        `json:"isTssAssortment"`
-	IsTstAssortment                 bool        `json:"isTstAssortment"`
-	IsTsvAssortment                 bool        `json:"isTsvAssortment"`
-	IsWebLaunch                     bool        `json:"isWebLaunch"`
-	OriginLevel1                    interface{} `json:"originLevel1"`
-	OriginLevel2                    interface{} `json:"originLevel2"`
-	OriginalPrice                   float32     `json:"originalPrice"`
-	OtherSelections                 interface{} `json:"otherSelections"`
-	PackagingLevel1                 string      `json:"packagingLevel1"`
-	Price                           float32     `json:"price"`
-	ProducerName                    string      `json:"producerName"`
-	ProductID                       string      `json:"productId"`
-	ProductLaunchDate               string      `json:"productLaunchDate"`
-	ProductNameBold                 string      `json:"productNameBold"`
-	ProductNameThin                 interface{} `json:"productNameThin"`
-	ProductNumber                   string      `json:"productNumber"`
-	ProductNumberShort              string      `json:"productNumberShort"`
-	RecycleFee                      int         `json:"recycleFee"`
-	RestrictedParcelQuantity        int         `json:"restrictedParcelQuantity"`
-	Seal                            interface{} `json:"seal"`
-	SellStartTime                   string      `json:"sellStartTime"`
-	SkatteverketType                string      `json:"skatteverketType"`
-	SugarContent                    int         `json:"sugarContent"`
-	SugarContentGramPer100Ml        int         `json:"sugarContentGramPer100ml"`
-	SupplierName                    string      `json:"supplierName"`
-	SystembolagetType               string      `json:"systembolagetType"`
-	SystembolagetUnitCut            float32     `json:"systembolagetUnitCut"`
-	SystembolagetPercentageCut      float32     `json:"systembolagetPercentageCut"`
-	Taste                           string      `json:"taste"`
-	TasteClockBitter                int         `json:"tasteClockBitter"`
-	TasteClockBody                  int         `json:"tasteClockBody"`
-	TasteClockCasque                int         `json:"tasteClockCasque"`
-	TasteClockFruitacid             int         `json:"tasteClockFruitacid"`
-	TasteClockGroupBitter           interface{} `json:"tasteClockGroupBitter"`
-	TasteClockGroupSmokiness        interface{} `json:"tasteClockGroupSmokiness"`
-	TasteClockRoughness             int         `json:"tasteClockRoughness"`
-	TasteClockSmokiness             int         `json:"tasteClockSmokiness"`
-	TasteClockSweetness             int         `json:"tasteClockSweetness"`
-	TasteClocks                     []struct {
-		Key   string `json:"key"`
-		Value int    `json:"value"`
-	} `json:"tasteClocks"`
-	TasteSymbols []string    `json:"tasteSymbols"`
-	Usage        string      `json:"usage"`
-	Vat          float32     `json:"moms"`
-	Vintage      interface{} `json:"vintage"`
-	Volume       int         `json:"volume"`
-	VolumeText   string      `json:"volumeText"`
+	AlcoholPercentage               float64       `json:"alcoholPercentage,omitempty" form:"alcoholPercentage"`
+	AlcoholTax                      float64       `json:"alcoholTax,omitempty" form:"alcoholTax"`
+	Assortment                      string        `json:"assortment,omitempty" form:"assortment"`
+	AssortmentText                  string        `json:"assortmentText,omitempty" form:"assortmentText"`
+	BottleText                      string        `json:"bottleText,omitempty" form:"bottleText"`
+	Category                        interface{}   `json:"category,omitempty" form:"category"`
+	CategoryLevel1                  string        `json:"categoryLevel1,omitempty" form:"categoryLevel1"`
+	CategoryLevel2                  string        `json:"categoryLevel2,omitempty" form:"categoryLevel2"`
+	CategoryLevel3                  string        `json:"categoryLevel3,omitempty" form:"categoryLevel3"`
+	CategoryLevel4                  interface{}   `json:"categoryLevel4,omitempty" form:"categoryLevel4"`
+	Color                           string        `json:"color,omitempty" form:"color"`
+	Country                         string        `json:"country,omitempty" form:"country"`
+	CustomCategoryTitle             string        `json:"customCategoryTitle,omitempty" form:"customCategoryTitle"`
+	DishPoints                      interface{}   `json:"dishPoints,omitempty" form:"dishPoints"`
+	EthicalLabel                    interface{}   `json:"ethicalLabel,omitempty" form:"ethicalLabel"`
+	Grapes                          []interface{} `json:"grapes,omitempty" form:"grapes"`
+	ImageModules                    ImageModule   `json:"imageModules,omitempty" form:"imageModules"`
+	Images                          []Image       `json:"images,omitempty" form:"images"`
+	IsAlcoholFree                   bool          `json:"isAlcoholFree,omitempty" form:"isAlcoholFree"`
+	IsBsAssortment                  bool          `json:"isBsAssortment,omitempty" form:"isBsAssortment"`
+	IsClimateSmartPackaging         bool          `json:"isClimateSmartPackaging,omitempty" form:"isClimateSmartPackaging"`
+	IsCompletelyOutOfStock          bool          `json:"isCompletelyOutOfStock,omitempty" form:"isCompletelyOutOfStock"`
+	IsDiscontinued                  bool          `json:"isDiscontinued,omitempty" form:"isDiscontinued"`
+	IsEthical                       bool          `json:"isEthical,omitempty" form:"isEthical"`
+	IsFsAssortment                  bool          `json:"isFsAssortment,omitempty" form:"isFsAssortment"`
+	IsFsTsAssortment                bool          `json:"isFsTsAssortment,omitempty" form:"isFsTsAssortment"`
+	IsKosher                        bool          `json:"isKosher,omitempty" form:"isKosher"`
+	IsManufacturingCountry          bool          `json:"isManufacturingCountry,omitempty" form:"isManufacturingCountry"`
+	IsNews                          bool          `json:"isNews,omitempty" form:"isNews"`
+	IsOrganic                       bool          `json:"isOrganic,omitempty" form:"isOrganic"`
+	IsPaAssortment                  bool          `json:"isPaAssortment,omitempty" form:"isPaAssortment"`
+	IsRecommendedByTasteProfile     bool          `json:"isRecommendedByTasteProfile,omitempty" form:"isRecommendedByTasteProfile"`
+	IsRegionalRestricted            bool          `json:"isRegionalRestricted,omitempty" form:"isRegionalRestricted"`
+	IsSupplierTemporaryNotAvailable bool          `json:"isSupplierTemporaryNotAvailable,omitempty" form:"isSupplierTemporaryNotAvailable"`
+	IsSustainableChoice             bool          `json:"isSustainableChoice,omitempty" form:"isSustainableChoice"`
+	IsTemporaryOutOfStock           bool          `json:"isTemporaryOutOfStock,omitempty" form:"isTemporaryOutOfStock"`
+	IsTsAssortment                  bool          `json:"isTsAssortment,omitempty" form:"isTsAssortment"`
+	IsTsLsAssortment                bool          `json:"isTsLsAssortment,omitempty" form:"isTsLsAssortment"`
+	IsTseAssortment                 bool          `json:"isTseAssortment,omitempty" form:"isTseAssortment"`
+	IsTssAssortment                 bool          `json:"isTssAssortment,omitempty" form:"isTssAssortment"`
+	IsTstAssortment                 bool          `json:"isTstAssortment,omitempty" form:"isTstAssortment"`
+	IsTsvAssortment                 bool          `json:"isTsvAssortment,omitempty" form:"isTsvAssortment"`
+	IsWebLaunch                     bool          `json:"isWebLaunch,omitempty" form:"isWebLaunch"`
+	OriginLevel1                    interface{}   `json:"originLevel1,omitempty" form:"originLevel1"`
+	OriginLevel2                    interface{}   `json:"originLevel2,omitempty" form:"originLevel2"`
+	OriginalPrice                   float32       `json:"originalPrice,omitempty" form:"originalPrice"`
+	OtherSelections                 interface{}   `json:"otherSelections,omitempty" form:"otherSelections"`
+	PackagingLevel1                 string        `json:"packagingLevel1,omitempty" form:"packagingLevel1"`
+	Price                           float32       `json:"price,omitempty" form:"price"`
+	ProducerName                    string        `json:"producerName,omitempty" form:"producerName"`
+	ProductID                       string        `json:"productId,omitempty" form:"productId"`
+	ProductLaunchDate               string        `json:"productLaunchDate,omitempty" form:"productLaunchDate"`
+	ProductNameBold                 string        `json:"productNameBold,omitempty" form:"productNameBold"`
+	ProductNameThin                 interface{}   `json:"productNameThin,omitempty" form:"productNameThin"`
+	ProductNumber                   string        `json:"productNumber,omitempty" form:"productNumber"`
+	ProductNumberShort              string        `json:"productNumberShort,omitempty" form:"productNumberShort"`
+	RecycleFee                      int           `json:"recycleFee,omitempty" form:"recycleFee"`
+	RestrictedParcelQuantity        int           `json:"restrictedParcelQuantity,omitempty" form:"restrictedParcelQuantity"`
+	Seal                            interface{}   `json:"seal,omitempty" form:"seal"`
+	SellStartTime                   string        `json:"sellStartTime,omitempty" form:"sellStartTime"`
+	SkatteverketType                string        `json:"skatteverketType,omitempty" form:"skatteverketType"`
+	SugarContent                    int           `json:"sugarContent,omitempty" form:"sugarContent"`
+	SugarContentGramPer100Ml        int           `json:"sugarContentGramPer100ml,omitempty" form:"sugarContentGramPer100ml"`
+	SupplierName                    string        `json:"supplierName,omitempty" form:"supplierName"`
+	SystembolagetType               string        `json:"systembolagetType,omitempty" form:"systembolagetType"`
+	SystembolagetUnitCut            float32       `json:"systembolagetUnitCut,omitempty" form:"systembolagetUnitCut"`
+	SystembolagetPercentageCut      float32       `json:"systembolagetPercentageCut,omitempty" form:"systembolagetPercentageCut"`
+	Taste                           string        `json:"taste,omitempty" form:"taste"`
+	TasteClockBitter                int           `json:"tasteClockBitter,omitempty" form:"tasteClockBitter"`
+	TasteClockBody                  int           `json:"tasteClockBody,omitempty" form:"tasteClockBody"`
+	TasteClockCasque                int           `json:"tasteClockCasque,omitempty" form:"tasteClockCasque"`
+	TasteClockFruitacid             int           `json:"tasteClockFruitacid,omitempty" form:"tasteClockFruitacid"`
+	TasteClockGroupBitter           interface{}   `json:"tasteClockGroupBitter,omitempty" form:"tasteClockGroupBitter"`
+	TasteClockGroupSmokiness        interface{}   `json:"tasteClockGroupSmokiness,omitempty" form:"tasteClockGroupSmokiness"`
+	TasteClockRoughness             int           `json:"tasteClockRoughness,omitempty" form:"tasteClockRoughness"`
+	TasteClockSmokiness             int           `json:"tasteClockSmokiness,omitempty" form:"tasteClockSmokiness"`
+	TasteClockSweetness             int           `json:"tasteClockSweetness,omitempty" form:"tasteClockSweetness"`
+	TasteClocks                     TasteClock    `json:"tasteClocks,omitempty" form:"tasteClocks"`
+	TasteSymbols                    []string      `json:"tasteSymbols,omitempty" form:"tasteSymbols"`
+	Usage                           string        `json:"usage,omitempty" form:"usage"`
+	Vat                             float32       `json:"moms,omitempty" form:"moms"`
+	Vintage                         interface{}   `json:"vintage,omitempty" form:"vintage"`
+	Volume                          int           `json:"volume,omitempty" form:"volume"`
+	VolumeText                      string        `json:"volumeText,omitempty" form:"volumeText"`
 }
 
 type VariableResponse struct {
@@ -219,7 +225,7 @@ func calcPerPercentageAndLiter(product *Product, taxInfo TaxInfo) {
 }
 
 func calcPerPureLiter(product *Product, taxInfo TaxInfo) {
-	product.AlcoholTax = float64(product.Volume/1000) * (product.AlcoholPercentage / 100) * taxInfo.TaxPerPercentAndLiter
+	product.AlcoholTax = (float64(product.Volume) / 1000) * (product.AlcoholPercentage / 100) * taxInfo.TaxPerPercentAndLiter
 }
 
 func findTaxLevel(product *Product, taxInfo []TaxInfo) TaxInfo {
